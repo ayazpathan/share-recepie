2f9e44cedbd50fb0e30d19af773b705b4e11f22e
ayazpathan <pathan.ayaz@outlook.com>
2021-08-30 16:53:48 +0530

    Working on fetching recipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,20 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function(){
+  try {
+    const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+    const data = await res.json();
+
+    if(!res.ok) throw new Error(`${data.message} ${res.status}`);
+
+    let {recipe} = data.data;
+  } catch (error) {
+    alert(error);
+  }
+}
+
+showRecipe();
+
+
